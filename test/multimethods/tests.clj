63b337a19d834d73c2d4d953c22634c5ccbdf3d2
Carlo Sciolla <skuro@skuro.tk>
2012-08-08 07:31:49 +0200

    Fixed tests

--- test/multimethods/tests.clj
+++ test/multimethods/tests.clj
@@ -14,13 +14,23 @@
        :default {:version :default}
        :default {:foo :bar}))
 
-; you can force types into  hierarchy
-(derive java.util.HashSet :multimethods/custom-type)
+; you can directly manipulate type hierarchies
+(derive java.util.Date :multimethods/custom-type)
+
+; final classes don't get in your way
+(derive String :multimethods/custom-type)
+
+; you can plug in new implementations
+(defmethod dispatch-by-object java.util.HashSet
+  [_]
+  "This is a Specialized Custom Type")
 
 (deftest test-dispatch-by-object
   (is
-   (isa? java.util.HashSet :multimethods/custom-type))
+   (isa? java.util.Date :multimethods/custom-type))
   (are [res arg] (= res (dispatch-by-object arg))
        "This is a Map" (java.util.TreeMap.)
        "This is a HashMap" (java.util.HashMap.)
-       "This is a Custom Type" (java.util.HashSet.)))
+       "This is a Custom Type" (java.util.Date.)
+       "This is a Custom Type" "foo"
+       "This is a Specialized Custom Type" (java.util.HashSet.)))
